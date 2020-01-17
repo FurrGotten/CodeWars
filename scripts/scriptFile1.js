4055729e4648b39728239e091eb5f0fcffdfe55d
FurrG <A2674151a>
2020-01-17 19:05:40 +0200

    Pig Latin

--- scripts/scriptFile1.js
+++ scripts/scriptFile1.js
@@ -22,6 +22,28 @@ function splitStrings(str){
 }
 
 
-console.log(splitStrings('abc'));
-console.log(splitStrings('abcdfg'));
+// console.log(splitStrings('abc'));
+// console.log(splitStrings('abcdfg'));
 
+
+//Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+//Leave punctuation marks untouched. /^[A-Za-z]+$/
+
+function pigIt(str){
+    let stringArray = [];
+    let separated = str.split(' ');
+    for (let i = 0; i < separated.length; i++) {
+        let doubleSepElem = separated[i].split('');
+        doubleSepElem.push(doubleSepElem[0]);
+        doubleSepElem.splice(0, 1);
+        let compareSpot = doubleSepElem[0].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"");
+        if (compareSpot.length !== 0){
+            doubleSepElem.push('ay');
+        }
+        stringArray.push(doubleSepElem.join(''));
+    }
+    return stringArray.join(' ');
+}
+
+console.log(pigIt('Pig latin is cool'));
+console.log(pigIt('Hello world !'));
